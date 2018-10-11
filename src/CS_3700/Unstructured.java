860d7f691066b344d771e73eb6748c72e56cc807
austin-m2 <morris.austinm@gmail.com>
2018-10-10 19:09:53 -0700

    Finished unstructured class

--- src/CS_3700/Unstructured.java
+++ src/CS_3700/Unstructured.java
@@ -1,36 +1,35 @@
 package CS_3700;
 
-public class Main {
+import java.util.concurrent.locks.ReentrantLock;
 
+public class Unstructured {
     public static void main(String[] args) {
-	// write your code here
-        Object forks[] = new Object[5];
+        ReentrantLock forks[] = new ReentrantLock[5];
         for (int i = 0; i < forks.length; i++) {
-            forks[i] = new Object();
+            forks[i] = new ReentrantLock();
         }
 
-        Philosopher A = new Philosopher(forks, 'A');
-        Philosopher B = new Philosopher(forks, 'B');
-        Philosopher C = new Philosopher(forks, 'C');
-        Philosopher D = new Philosopher(forks, 'D');
-        Philosopher E = new Philosopher(forks, 'E');
+        Philosopher1 A = new Philosopher1(forks, 'A');
+        Philosopher1 B = new Philosopher1(forks, 'B');
+        Philosopher1 C = new Philosopher1(forks, 'C');
+        Philosopher1 D = new Philosopher1(forks, 'D');
+        Philosopher1 E = new Philosopher1(forks, 'E');
 
         A.start();
         B.start();
         C.start();
         D.start();
         E.start();
-
     }
 }
 
-class Philosopher extends Thread {
-    Object forks[];
-    Object leftFork;
-    Object rightFork;
+class Philosopher1 extends Thread {
+    ReentrantLock forks[];
+    ReentrantLock leftFork;
+    ReentrantLock rightFork;
     char name;
 
-    Philosopher(Object forks[], char name) {
+    Philosopher1(ReentrantLock forks[], char name) {
         this.forks = forks;
         this.name = name;
         switch (name) {
@@ -51,9 +50,9 @@ class Philosopher extends Thread {
                 rightFork = forks[4];
                 break;
             case 'E':
-                //Order is flipped for this philosopher
-                leftFork = forks[0];
-                rightFork = forks[4];
+                //Order is NOT flipped for this philosopher
+                leftFork = forks[4];
+                rightFork = forks[0];
                 break;
             default:
                 System.out.println("Invalid philosopher name!");
@@ -61,33 +60,38 @@ class Philosopher extends Thread {
         }
     }
 
-
     @Override
     public void run() {
         int time;
         while (true) {
             try {
                 System.out.println("Philosopher " + name + ": attempt to acquire fork to left");
-                synchronized (leftFork) {
+                if (leftFork.tryLock()) {
                     System.out.println("Philosopher " + name + ": acquired left fork");
                     System.out.println("Philosopher " + name + ": attempt to acquire fork to right");
-                    synchronized (rightFork) {
+                    if (rightFork.tryLock()) {
                         System.out.println("Philosopher " + name + ": acquired right fork");
                         time = (int) (Math.random() * 10 + 1);
                         System.out.println("Philosopher " + name + ": eating for " + time + " seconds");
                         sleep(time * 1000);
+                        System.out.println("Philosopher " + name + ": finished eating! dropping both forks");
+                        rightFork.unlock();
+                        leftFork.unlock();
+                    } else {
+                        System.out.println("Philosopher " + name + ": right fork is in use! Dropping left fork");
+                        leftFork.unlock();
                     }
-                    System.out.println("Philosopher " + name + ": dropped right fork");
+                } else {
+                    System.out.println("Philosopher " + name + ": left fork is in use! ");
                 }
-                System.out.println("Philosopher " + name + ": dropped left fork");
+
                 time = (int) (Math.random() * 10 + 1);
-                System.out.println("Philosopher " + name + ": Thinking for " + time + " seconds");
+                System.out.println("Philosopher " + name + ": thinking for " + time + " seconds");
                 sleep(time * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
-
     }
+
 }
